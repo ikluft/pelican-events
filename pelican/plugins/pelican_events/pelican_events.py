6811fd38242af0061e4dd430a222312cbc88a870
Ian Kluft <ko6yq.dev+github@gmail.com>
2025-09-06 23:25:01 -0700

    process GEO field, remove redundant lower() calls

--- pelican/plugins/pelican_events/pelican_events.py
+++ pelican/plugins/pelican_events/pelican_events.py
@@ -23,6 +23,7 @@ from dateutil import rrule
 import dateutil.parser
 import html2text
 import icalendar
+from icalendar.prop import vGeo
 from recurrent.event_parser import RecurringEvent
 
 from pelican import contents, signals
@@ -296,22 +297,34 @@ def xfer_metadata_to_event(metadata: dict[str, Any] | None, event: icalendar.cal
     for field in iter(metadata):
         if field.lower().startswith("event-"):
             fname = field[6:].lower()
+            log.debug("processing field %s", fname)
 
             # save comment property for later, append errors at end if any
-            if fname.lower() == 'comment':
+            if fname == 'comment':
                 comment.append(metadata[field])
+                log.debug("field saved as comment")
                 continue
 
             # skip start, end and duration because they are generated internally
-            if fname not in ["start", "end", "duration"]:
+            if fname in ["start", "end", "duration"]:
+                log.debug("field %s skipped because it is processed separately", fname)
                 continue
 
             # skip disallowed properties, add note to errors list for report in comment property
             status = field_name_check(fname)
             if status is not None:
                 errors.append(status)
+                log.debug("field %s skipped because of error %s", fname, status)
+                continue
 
-            event.add(fname.lower(), metadata[field])
+            # special handling for "GEO" geographic coordinates
+            if fname == 'geo':
+                geo_text = metadata[field]
+                event.add('geo', vGeo.from_ical(geo_text))
+                log.debug("field %s processed as coordinates %s", fname, geo_text)
+                continue
+
+            event.add(fname, metadata[field])
 
     # process comment property combining user text with any errors that may have occurred
     if len(errors) > 0:
