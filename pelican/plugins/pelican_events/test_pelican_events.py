55a0eafe3a9e1416ba1c06badc484c3538902105
Ian Kluft <ko6yq.dev+github@gmail.com>
2025-09-09 18:17:10 -0700

    apply lint fixes to new test code

--- pelican/plugins/pelican_events/test_pelican_events.py
+++ pelican/plugins/pelican_events/test_pelican_events.py
@@ -5,12 +5,12 @@ import unittest
 import pelican.plugins.pelican_events
 
 TEST_CASES = {
-        'strip_html_tags': (
-            {'in': 'no HTML here', 'out': 'no HTML here'},
-            {'in': '<i>italic</i>', 'out': '_italic_'},
-            {'in': '<b>bold</b>', 'out': '**bold**'},
-        ),
-    }
+    "strip_html_tags": (
+        {"in": "no HTML here", "out": "no HTML here"},
+        {"in": "<i>italic</i>", "out": "_italic_"},
+        {"in": "<b>bold</b>", "out": "**bold**"},
+    ),
+}
 
 
 class TestCaseSet(unittest.TestCase):
@@ -28,13 +28,18 @@ class TestStripHtmlTags(TestCaseSet):
     @classmethod
     def case_set(cls):
         """Return name of test case set."""
-        return 'strip_html_tags'
+        return "strip_html_tags"
 
     def test_strip_html_tags(self):
         """Subtests for strip_html_tags()."""
         for test_num, test_case in enumerate(TEST_CASES[self.__class__.case_set()]):
-            with self.subTest(type=self.__class__.case_set(), number=test_num, text=test_case['in']):
-                self.assertEqual(pelican.plugins.pelican_events.strip_html_tags(test_case['in']), test_case['out'])
+            with self.subTest(
+                type=self.__class__.case_set(), number=test_num, text=test_case["in"]
+            ):
+                self.assertEqual(
+                    pelican.plugins.pelican_events.strip_html_tags(test_case["in"]),
+                    test_case["out"],
+                )
 
 
 if __name__ == "__main__":
